ccbb65778b4fbe3a038431e4506a2d1d6b595503
Cris Mihalache <me@f3rno.com>
2018-08-09 16:42:04 +0300

    (refactor) complete split into functions

--- examples/server.js
+++ examples/server.js
@@ -2,27 +2,55 @@
 
 process.env.DEBUG = 'hf:*'
 
+require('bfx-hf-util/lib/catch_uncaught_errors')
+
+const PI = require('p-iteration')
 const debug = require('debug')('hf:algo-server:examples:server')
-const { IcebergOrder, TWAPOrder, MarketMakerOrder } = require('bfx-hf-algo')
 const { onExit: OnExit } = require('bfx-hf-util')
+const {
+  IcebergOrder, TWAPOrder, MarketMakerOrder
+} = require('bfx-hf-algo')
 
 const bfx = require('./bfx')
-const AlgoServer = require('../lib/server')
+const startAlgoServer = require('../lib/start')
+const registerAlgos = require('../lib/ui/register_algos')
+const startAllOrders = require('../lib/orders/start_all')
+const onWSMessage = require('../lib/ws/events/on_message')
+const stopHB = require('../lib/ws/hb/stop')
+const saveOrder = require('../lib/orders/save')
 
-let server
-const UPDATE_UI = false
+const UPDATE_UI = true
 const rest = bfx.rest(2)
 const ws = bfx.ws(2, {
   manageOrderBooks: true
 })
 
+let as = null // algo server state
+
 OnExit(() => (ws && ws.isOpen()) && ws.close(), -2)
-OnExit(() => (server && server.isActive()) && server.stop(), -1)
+OnExit(async () => { // stop heartbeat
+  if (as === null) {
+    return
+  }
+
+  if (as.hbi !== null) {
+    as = stopHB(as)
+  }
+
+  await PI.forEach(as.orders, async (o) => saveOrder(o))
+}, -1)
 
 ws.on('error', debug)
+ws.on('open', () => {
+  debug('bfx ws client connected')
+  ws.auth(0, 4) // note active DMS
+})
 
-const run = async () => {
-  server = new AlgoServer({
+ws.on('auth', async () => {
+  debug('bfx ws client authenticated')
+
+  as = await startAlgoServer({
+    id: 'example',
     algos: {
       ao_market_maker: MarketMakerOrder,
       ao_iceberg: IcebergOrder,
@@ -33,22 +61,15 @@ const run = async () => {
   })
 
   if (UPDATE_UI) {
-    await server.registerAlgos()
+    await registerAlgos(as)
+    debug('registered algo order UI layouts')
   }
 
-  ws.open()
-  ws.on('open', ws.auth.bind(ws, 0, 4))
-  ws.on('auth', async () => {
-    await server.start()
-  })
-}
+  await startAllOrders(as)
 
-try {
-  run().then(() => {
-    debug('server running')
-  }).catch((e) => {
-    debug('error: %s', e.message)
+  ws.onMessage({}, async (msg) => {
+    as = await onWSMessage(as, msg)
   })
-} catch (e) {
-  debug(e.stack)
-}
+})
+
+ws.open()
