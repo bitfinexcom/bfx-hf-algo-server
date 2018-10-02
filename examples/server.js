b78a07b1feb798309610ff778e458f41ff58dd49
Cris Mihalache <me@f3rno.com>
2018-10-02 18:47:14 +0300

    (refactor) rm old server, mv new one out of experimental dir

--- examples/server.js
+++ examples/server.js
@@ -2,23 +2,43 @@
 
 process.env.DEBUG = '*' // 'bfx:hf:*'
 
+require('dotenv').config()
 require('bfx-hf-util/lib/catch_uncaught_errors')
 
 const debug = require('debug')('bfx:hf:algo-server:examples:server')
+const SocksProxyAgent = require('socks-proxy-agent')
 const { IcebergOrder, TWAPOrder, AccumulateDistribute } = require('bfx-hf-algo')
-const AOServer = require('../lib/experimental/server')
-const { args: aoServerArgs } = require('./bfx')
+const { connectDB, startDB } = require('bfx-hf-models')
 
-const server = new AOServer({
-  ...aoServerArgs,
-  aos: [IcebergOrder, TWAPOrder, AccumulateDistribute],
-  port: 8877,
-})
+const AOServer = require('../lib/server')
+const { API_KEY, API_SECRET, WS_URL, REST_URL, SOCKS_PROXY_URL } = process.env
 
-server.on('auth:success', () => {
-  debug('authenticated')
-})
+const run = async () => {
+  await startDB(`${__dirname}/../db`)
+  await connectDB('hf-ds')
 
-server.on('auth:error', (error) => {
-  debug('auth error: %j', error)
-})
+  const server = new AOServer({
+    apiKey: API_KEY,
+    apiSecret: API_SECRET,
+    wsURL: WS_URL,
+    restURL: REST_URL,
+    agent: SOCKS_PROXY_URL ? new SocksProxyAgent(SOCKS_PROXY_URL) : null,
+  
+    aos: [IcebergOrder, TWAPOrder, AccumulateDistribute],
+    port: 8877,
+  })
+
+  server.on('auth:success', () => {
+    debug('authenticated')
+  })
+
+  server.on('auth:error', (error) => {
+    debug('auth error: %j', error)
+  })
+}
+
+try {
+  run()
+} catch (err) {
+  debug('error: %s', err)
+}
