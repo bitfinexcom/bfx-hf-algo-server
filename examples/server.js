c16ca9a2cd17bd59e246388216867ebcab5b2914
Cris Mihalache <me@f3rno.com>
2019-09-01 16:35:00 +0700

    (refactor) update to use new AOAdapter system

--- examples/server.js
+++ examples/server.js
@@ -8,40 +8,55 @@ require('bfx-hf-util/lib/catch_uncaught_errors')
 const debug = require('debug')('bfx:hf:algo-server:examples:server')
 const SocksProxyAgent = require('socks-proxy-agent')
 const {
-  PingPong, Iceberg, TWAP, AccumulateDistribute
+  PingPong, Iceberg, TWAP, AccumulateDistribute, MACrossover
 } = require('bfx-hf-algo')
 
+const HFDB = require('bfx-hf-models')
+const HFDBLowDBAdapter = require('bfx-hf-models-adapter-lowdb')
+const {
+  AOAdapter,
+  schema: HFDBBitfinexSchema
+} = require('bfx-hf-ext-plugin-bitfinex')
+
 const AOServer = require('../lib/server')
-const { API_KEY, API_SECRET, WS_URL, REST_URL, SOCKS_PROXY_URL } = process.env
+const {
+  API_KEY, API_SECRET, WS_URL, REST_URL, SOCKS_PROXY_URL, DB_FILENAME
+} = process.env
 
-const run = async () => {
-  const server = new AOServer({
-    apiKey: API_KEY,
-    apiSecret: API_SECRET,
-    wsURL: WS_URL,
-    restURL: REST_URL,
-    agent: SOCKS_PROXY_URL ? new SocksProxyAgent(SOCKS_PROXY_URL) : null,
-    port: 8877,
-
-    aos: [
-      PingPong,
-      Iceberg,
-      TWAP,
-      AccumulateDistribute
-    ],
+const db = new HFDB({
+  schema: HFDBBitfinexSchema,
+  adapter: HFDBLowDBAdapter({
+    dbPath: `${__dirname}/../${DB_FILENAME}`,
+    schema: HFDBBitfinexSchema
   })
+})
 
-  server.on('auth:success', () => {
-    debug('authenticated')
-  })
+const adapter = new AOAdapter({
+  apiKey: API_KEY,
+  apiSecret: API_SECRET,
+  wsURL: WS_URL,
+  restURL: REST_URL,
+  agent: SOCKS_PROXY_URL ? new SocksProxyAgent(SOCKS_PROXY_URL) : null,
+  withHeartbeat: true
+})
 
-  server.on('auth:error', (error) => {
-    debug('auth error: %j', error)
-  })
-}
+const server = new AOServer({
+  db,
+  adapter,
+  port: 8877,
+  aos: [
+    PingPong,
+    Iceberg,
+    TWAP,
+    AccumulateDistribute,
+    MACrossover
+  ]
+})
 
-try {
-  run()
-} catch (err) {
-  debug('error: %s', err)
-}
+server.on('auth:success', () => {
+  debug('authenticated')
+})
+
+server.on('auth:error', (error) => {
+  debug('auth error: %j', error)
+})
